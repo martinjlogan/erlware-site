6607aaacb1a5045605f0d721ef62972a904e60b8
Samuel Rivas <samuelrivas@gmail.com>
2008-05-22 00:44:31 +0200

    support for testing in update-site.sh

--- scripts/update-site.sh
+++ scripts/update-site.sh
@@ -1,14 +1,27 @@
 #!/bin/sh
 
-
 # be paranoid
 set -e
 
 export PATH=$PATH:/usr/bin:/usr/local/bin:/bin
-export SITE_HOME=/usr/local/scripts/site
+
+# Define common places
+if [ "$1" == "--test" ]; then
+    SITE_HOME=/tmp/erlware_site_test
+    WWW_HOME=/tmp/erlware_site_test/www
+else
+    SITE_HOME=/usr/local/scripts/site
+    WWW_HOME=/usr/local/www
+fi
+
+# Update the local repository
 cd $SITE_HOME/erlware-site
+
 logger -t erlware-site "pulling from canonical"
+
 git pull
+
+# Generate the local copy
 logger -t erlware-site "generating site"
 webgen run
 
@@ -26,5 +39,5 @@ fi
 
 # Publish the site
 logger -t erlware-site "rsyncing"
-rsync -vaz $SITE_HOME/erlware-site/output/ /usr/local/www/erlware/
+rsync -vaz $SITE_HOME/erlware-site/output/ $WWW_HOME/erlware
 logger -t erlware-site "erlware.org updated"
